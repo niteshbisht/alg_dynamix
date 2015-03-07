fecf7336629d35d3122d7c1c0e50ff56c308116e
niteshbisht <nitesh_bisht@yahoo.com>
2015-03-07 22:55:56 +0530

    <nitesh.bisht> initialize the alg dynamix repo

---
+++ src/main/java/com/nitesh/fibnseq/Fibonacci.java
@@ -0,0 +1,102 @@
+package com.nitesh.fibnseq;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+ 
+public class Fibonacci {
+    private static long[] value;
+ 
+    public static void main(String args[]) throws InterruptedException {
+        int n;
+        try {
+            n = 20;
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Please enter in the form java n number ");
+        }
+        value = new long[n + 1];
+        long start = System.nanoTime();
+        System.out.print("Dynamic Programming = " + fibon(n));
+        long end = System.nanoTime();
+        System.out.println("\t time = " + (end - start) + "ns");
+        start = System.nanoTime();
+        System.out.print("Sequence = " + Sequence(n));
+        end = System.nanoTime();
+        System.out.println("\t time = " + (end - start) + "ns");
+        start = System.nanoTime();
+        //int nThreads = Runtime.getRuntime().availableProcessors();
+        int nThreads = 30;
+        System.out.print(nThreads);
+        ExecutorService executorService = Executors
+                .newFixedThreadPool(nThreads);
+        int result;
+        try {
+            result = fibonacciSum(n, executorService);
+        } catch (ExecutionException e) {
+            throw new RuntimeException("Thread Interuppted ");
+        }
+        System.out.print(" MultiThreading = " + result);
+        end = System.nanoTime();
+        System.out.println("\t time = " + (end - start) + "ns");
+    }
+ 
+    public static long fibon(int n) {
+ 
+        value[0] = 1;
+        value[1] = 1;
+        if (n <= 2)
+            return 1;
+        else if (value[n - 1] != 0)
+            return value[n];
+ 
+        for (int j = 2; j <= n; j++) {
+            value[j] = fibon(j - 2) + fibon(j - 1);
+        }
+        return value[n];
+    }
+ 
+    public static long Sequence(int n) {
+        if (n <= 2)
+            return 1;
+        else
+            return (Sequence(n - 1) + Sequence(n - 2));
+ 
+    }
+ 
+    private static class FibonacciThread implements Runnable {
+        int index;
+        int result;
+        ExecutorService executorService;
+ 
+        public FibonacciThread(int index) {
+            this.index = index;
+        }
+ 
+        public void run() {
+            try {
+                this.result = fibonacciSum(index, executorService);
+            } catch (Exception e) {
+                throw new RuntimeException("Thread interupted");
+            }
+        }
+    }
+ 
+    private static int fibonacciSum(int index, ExecutorService executorService)
+            throws InterruptedException, ExecutionException {
+        if (index == 1 || index == 2) {
+            return 1;
+        } else {
+             
+            FibonacciThread fibonacciThread1 = new FibonacciThread(index - 2);
+            fibonacciThread1.executorService=executorService;
+            Future future = executorService.submit(fibonacciThread1);
+            Object object = future.get();
+            int resultPart2 = fibonacciSum(index - 1, executorService);
+            int result = fibonacciThread1.result + resultPart2;
+            //executorService.shutdown();
+            return result;
+        }
+    }
+ 
+}
---
+++ src/test/java/com/nitesh/reverse/ptrar/PlaywithPointersTest.java
@@ -0,0 +1,36 @@
+package com.nitesh.reverse.ptrar;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlaywithPointersTest {
+
+	@Test
+	public void testPlayWithPointers() {
+		PlaywithPointers ppt=new PlaywithPointers();
+		CharPtr m =ppt.playPointers("Hello");
+		
+		String resultstr=ppt.viewChars(m);
+		assertEquals("Hello", resultstr);
+		
+	}
+	
+	@Test
+	public void testPlayWithPointersReversal() {
+		PlaywithPointers ppt=new PlaywithPointers();
+		CharPtr m =ppt.playPointers("IOU");
+		
+		CharPtr p=ppt.reverseObject(m);
+		CharPtr n=ppt.recurseReversal(p, null);
+		String resultstri=ppt.viewChars(n);
+		System.out.println(resultstri);
+		String resultstr=ppt.viewChars(p);
+		assertEquals("UOI", resultstr);
+		ArrayList<Integer> p2 = new ArrayList<>();
+		LinkedList<Integer> p3=new LinkedList<>();
+	}
+}
