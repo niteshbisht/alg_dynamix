7667f00abf5b5eda0cc255eab6d5c017e8533458
sysxcomintl <sysxcomintl@sysxcom>
2015-07-26 12:54:37 +0530

    "main"

---
+++ java_design_patterns/src/main/java/com/zbase/design/engine/main/MainCl.java
@@ -0,0 +1,11 @@
+package com.zbase.design.engine.main;
+
+import com.design.pattern.observer.ObserverUtil;
+
+public class MainCl {
+
+	public static void main(String[] args) {
+		ObserverUtil.invokeObserverCode();
+	}
+
+}
