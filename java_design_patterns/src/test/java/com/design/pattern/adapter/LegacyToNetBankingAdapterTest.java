b8b40768e49bd9c438d8f9b39f86fdf63d74a06c
sysxcomintl <sysxcomintl@sysxcom>
2015-07-26 14:56:11 +0530

    "test case adapter"

--- java_design_patterns/src/test/java/com/design/pattern/adapter/LegacyToNetBankingAdapterTest.java
+++ java_design_patterns/src/test/java/com/design/pattern/adapter/LegacyToNetBankingAdapterTest.java
@@ -1,5 +1,17 @@
 package com.design.pattern.adapter;
 
-public class LegacyToNetBankingAdapterTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class LegacyToNetBankingAdapterTest {
+	
+	@Test
+	public void testLegacyWithNetBank(){
+		LegacyBanking lbs = new LegacyBanking();
+		lbs.setAccountNumber(12152);
+		lbs.setName("alchemy roosevelt");
+		LegacyToNetBankingAdapter lbtnbadpt = new LegacyToNetBankingAdapter(lbs, 16213);
+		Assert.assertTrue("matches first name is valid", lbtnbadpt.getFirstName().equals("alchemy"));
+		Assert.assertTrue("matches last name is valid", lbtnbadpt.getLastName().equals("roosevelt"));
+	}
 }
