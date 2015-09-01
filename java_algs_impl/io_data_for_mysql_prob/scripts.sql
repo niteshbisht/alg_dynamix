12c20ef2947512f4c7e9929b13ad5effe285fcc4
sysxcomintl <sysxcomintl@sysxcom>
2015-09-02 02:28:43 +0530

    "updated files"

--- java_algs_impl/io_data_for_mysql_prob/scripts.sql
+++ java_algs_impl/io_data_for_mysql_prob/scripts.sql
@@ -8,6 +8,6 @@ select * from tree;
 insert into tree values ('Bussiness Group','Information System','Info Sec Group','Authentication Group','Datacenter Group','Certificate Group','Tokens Group');
 insert into tree values ('Bussiness Group','Information System','Info Sec Group','Authentication Group','DatawareHouse Group','Certificate Group','Tokens Group');
 insert into tree values ('Bussiness Group','Information System','Info Sec Admin','Authentication Group','Facilities Group','Certificate Group','Tokens Group');
-insert into tree values ('Wealth Management','Information System','Infrastructure Team','Store Team','Network Team','Finance Team','Records Team');
+insert into tree values ('Wealth Management','Midsider System','Infrastructure Team','Store Team','Network Team','Finance Team','Records Team');
 insert into tree values ('Wealth Management','Parallel System','SSL Support Team','Device Support','Helpdesk Team','24x7 Support Team','Routing Support');
-0.912493 millisecs
+1025882 nanosecs
