c3c1d67b4c10b3457461d59ea8a9ba2c3003149e
sysxcomintl <sysxcomintl@sysxcom>
2015-09-02 02:04:59 +0530

    "added io counterparts"

---
+++ java_algs_impl/io_data_for_mysql_prob/scripts.sql
@@ -0,0 +1,12 @@
+show databases;
+use sqlpract;
+
+create table tree(col1 varchar(30),col2 varchar(30),col3 varchar(30),col4 varchar(30),col5 varchar(30),col6 varchar(30),col7 varchar(30));
+truncate table tree;
+select * from tree;
+
+insert into tree values ('Bussiness Group','Information System','Info Sec Group','Authentication Group','Datacenter Group','Certificate Group','Tokens Group');
+insert into tree values ('Bussiness Group','Information System','Info Sec Group','Authentication Group','DatawareHouse Group','Certificate Group','Tokens Group');
+insert into tree values ('Bussiness Group','Information System','Info Sec Admin','Authentication Group','Facilities Group','Certificate Group','Tokens Group');
+insert into tree values ('Wealth Management','Information System','Infrastructure Team','Store Team','Network Team','Finance Team','Records Team');
+insert into tree values ('Wealth Management','Parallel System','SSL Support Team','Device Support','Helpdesk Team','24x7 Support Team','Routing Support');
