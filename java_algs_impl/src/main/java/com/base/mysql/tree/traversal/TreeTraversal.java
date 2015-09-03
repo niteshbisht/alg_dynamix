1538876fd4b61bfb457d67dca113a8c4cd35049a
sysxcomintl <sysxcomintl@sysxcom>
2015-09-04 00:31:54 +0530

    "added the logic for duplicated data"

--- java_algs_impl/src/main/java/com/base/mysql/tree/traversal/TreeTraversal.java
+++ java_algs_impl/src/main/java/com/base/mysql/tree/traversal/TreeTraversal.java
@@ -1,5 +1,8 @@
 package com.base.mysql.tree.traversal;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -25,35 +28,60 @@ public class TreeTraversal {
 		treeTrav.getData();
 	}
 
-	public void getDataJsonFromResultSet(ResultSet rs) throws SQLException {
+	public void getDataJsonFromResultSet(ResultSet rs) throws SQLException,
+			FileNotFoundException {
+		File fi = new File("io_data_for_mysql_prob/data.json");
+		FileOutputStream fo = new FileOutputStream(fi);
+
 		String currentLevelName;
 		TreeStructure rootNode = new TreeStructure();
 		rootNode.setLevelIndex(0);
+		rootNode.setLevelName("root");
 		TreeStructure childNode;
 		String parentLevelName;
 		int i = 1;
 		while (rs.next()) {
 			i = 1;
-			while (i <= 7) {
-				currentLevelName = rs.getString(i);
-				boolean present = searchRecordInTree(rootNode,
-						currentLevelName, i);
-				if (!present) {
-					if (i == 1)
-						parentLevelName = null;
-					else
-						parentLevelName = rs.getString(i - 1);
-					childNode = createTreeNode(i, currentLevelName);
-					putdataInTree(rootNode, childNode, currentLevelName,
-							parentLevelName, i);
+			boolean dupCol = ResultSetUtil.getDuplicacy(rs);
+			if (dupCol) {
+				Map<String, List<Integer>> mm = ResultSetUtil.getDataDupls(rs);
+				while (i <= 7) {
+					currentLevelName = rs.getString(i);
+					boolean present = searchRecordInTree(rootNode,
+							currentLevelName, i);
+					if (!present) {
+						if (i == 1)
+							parentLevelName = null;
+						else
+							parentLevelName = rs.getString(i - 1);
+						childNode = createTreeNode(i, currentLevelName);
+						putdataInTreeInRedundantCase(rootNode, childNode,
+								i - 1, parentLevelName);
+					}
+					i++;
 				}
-				i++;
-			}
+			} else
+				while (i <= 7) {
+					currentLevelName = rs.getString(i);
+					boolean present = searchRecordInTree(rootNode,
+							currentLevelName, i);
+					if (!present) {
+						if (i == 1)
+							parentLevelName = null;
+						else
+							parentLevelName = rs.getString(i - 1);
+						childNode = createTreeNode(i, currentLevelName);
+						putdataInTree(rootNode, childNode, currentLevelName,
+								parentLevelName, i);
+					}
+					i++;
+				}
 		}
 		ObjectMapper om = new ObjectMapper();
 		try {
 			String s = om.writeValueAsString(rootNode);
-			System.out.println(s);
+			fo.write(s.getBytes());
+			fo.flush();
 		} catch (JsonGenerationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -63,6 +91,80 @@ public class TreeTraversal {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+
+	private void putdataInTreeInRedundantCase(TreeStructure rootNode,
+			TreeStructure childNode, int levelIndexParent,
+			String parentLevelName) {
+		if (null == parentLevelName) {
+			if (rootNode.getChildNodes().size() == 0) {
+				List<TreeStructure> listTs = new ArrayList<TreeStructure>();
+				listTs.add(childNode);
+				rootNode.getChildNodes().put(childNode.getLevelName(), listTs);
+			} else {
+				Map<String, List<TreeStructure>> tsMap = rootNode
+						.getChildNodes();
+				boolean b = tsMap.containsKey(childNode.getLevelName());
+				if (b) {
+					List<TreeStructure> listTs = tsMap.get(childNode
+							.getLevelName());
+					for (int i = 0; i < listTs.size(); i++) {
+						TreeStructure te = listTs.get(i);
+						if (te.getLevelName().equals(childNode.getLevelName())
+								&& te.getLevelIndex() == childNode
+										.getLevelIndex()) {
+							break;
+						}
+					}
+				} else {
+					List<TreeStructure> listTs = new ArrayList<TreeStructure>();
+					listTs.add(childNode);
+					tsMap.put(childNode.getLevelName(), listTs);
+				}
+			}
+		} else {
+			if (rootNode.getLevelName().equals(childNode.getLevelName())
+					&& rootNode.getLevelIndex() == levelIndexParent) {
+				boolean presence = rootNode.getChildNodes().containsKey(
+						childNode.getLevelName());
+				if (presence) {
+					List<TreeStructure> ts = rootNode.getChildNodes().get(
+							childNode.getLevelName());
+					boolean internalPresence = false;
+					for (int i = 0; i < ts.size(); i++) {
+						TreeStructure tempNode = ts.get(i);
+						if (tempNode.getLevelName().equals(
+								childNode.getLevelName())
+								&& tempNode.getLevelIndex() == childNode
+										.getLevelIndex()) {
+							internalPresence = true;
+						}
+					}
+					if (!internalPresence)
+						ts.add(childNode);
+				} else {
+					List<TreeStructure> ts = new ArrayList<TreeStructure>();
+					ts.add(childNode);
+					rootNode.getChildNodes().put(childNode.getLevelName(), ts);
+				}
+
+			} else {
+				Map<String, List<TreeStructure>> tsMap = rootNode
+						.getChildNodes();
+				Iterator<String> itr = tsMap.keySet().iterator();
+				while (itr.hasNext()) {
+					List<TreeStructure> ts = tsMap.get(itr.next());
+					for (int i = 0; i < ts.size(); i++) {
+						TreeStructure treeStruct = ts.get(i);
+						if (levelIndexParent < treeStruct.getLevelIndex())
+							break;
+						putdataInTreeInRedundantCase(treeStruct, childNode,
+								levelIndexParent, parentLevelName);
+
+					}
+				}
+			}
 		}
 	}
 
@@ -76,28 +178,33 @@ public class TreeTraversal {
 			if (tsListMap.containsKey(parentLevelName)) {
 				List<TreeStructure> tsListItems = tsListMap
 						.get(parentLevelName);
-				for(int i = 0;i<tsListItems.size();i++){
+				for (int i = 0; i < tsListItems.size(); i++) {
 					TreeStructure ts = tsListItems.get(i);
-					if(ts.getLevelName().equals(parentLevelName) && ts.getLevelIndex()==levelIndex-1){
-						Map<String, List<TreeStructure>> tsChild = ts.getChildNodes();
+					if (ts.getLevelName().equals(parentLevelName)
+							&& ts.getLevelIndex() == levelIndex - 1) {
+						Map<String, List<TreeStructure>> tsChild = ts
+								.getChildNodes();
 						List<TreeStructure> addList = new ArrayList<TreeStructure>();
 						addList.add(childNode);
 						tsChild.put(currentLevelName, addList);
 					}
 				}
-			}else{
+			} else {
 				Iterator<String> itr = tsListMap.keySet().iterator();
 				while (itr.hasNext()) {
 					List<TreeStructure> tsl = tsListMap.get(itr.next());
-					for(int i = 0;i<tsl.size();i++){
+					for (int i = 0; i < tsl.size(); i++) {
 						TreeStructure ts = tsl.get(i);
-						if(ts.getLevelName().equals(parentLevelName) && ts.getLevelIndex()==levelIndex-1){
-							Map<String, List<TreeStructure>> tsChild = ts.getChildNodes();
+						if (ts.getLevelName().equals(parentLevelName)
+								&& ts.getLevelIndex() == levelIndex - 1) {
+							Map<String, List<TreeStructure>> tsChild = ts
+									.getChildNodes();
 							List<TreeStructure> addList = new ArrayList<TreeStructure>();
 							addList.add(childNode);
 							tsChild.put(currentLevelName, addList);
-						}else{
-							putdataInTree(ts, childNode, currentLevelName, parentLevelName, levelIndex);
+						} else {
+							putdataInTree(ts, childNode, currentLevelName,
+									parentLevelName, levelIndex);
 						}
 					}
 				}
